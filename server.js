21d55960808b3866075cbf90faecb5b866c13b98
nancysangani <9d.24.nancy.sangani@gmail.com>
2026-02-11 00:57:25 +0530

    refactor: update server.js for Vercel compatibility

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import fetch from 'node-fetch';
+import express from "express";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/weather', async(req, resp) => {
+app.get("/api/weather", async (req, resp) => {
   const city = req.query.city;
   const apiKey = process.env.WEATHER_API_KEY;
 
-  if(!city) {
-    return resp.status(400).json({ error: 'City name is required' });
+  if (!city) {
+    return resp.status(400).json({ error: "City name is required" });
   }
   try {
     const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=7&aqi=yes`;
@@ -22,10 +22,12 @@ app.get('/weather', async(req, resp) => {
     const data = await apiResponse.json();
     resp.json(data);
   } catch (error) {
-    resp.status(500).json({ error: 'Failed to fetch weather data' });
+    resp.status(500).json({ error: "Failed to fetch weather data" });
   }
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+export default app;
